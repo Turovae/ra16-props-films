0995a525c5b3f0512289f9198f075e0abd05774f
Turovae <ekaterina.turova@mail.ru>
2023-05-17 16:53:49 +0700

    add stars component

--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -1,7 +1,32 @@
-function Stars(props) {
+import Star from "./Star/Star";
+import './Stars.css';
+
+interface StarsProp {
+  count: any;
+}
+
+function Stars(props: StarsProp): JSX.Element | null {
+  const { count } = props;
+  if (count < 1 || count > 5) {
+    return null;
+  }
+
+  if (typeof count !== 'number') {
+    return null;
+  }
+
+  if (isNaN(count)) {
+    return null;
+  }
+
   return (
     <>
-      <div>This is stars component with {props.count} stars</div>
+      <div>This is stars component with { count } stars</div>
+      <ul className="card-body-stars u-clearfix">
+        { [...Array(count).keys()].map((key) => <Star key={key} />) }
+      </ul>
     </>
   )
 }
+
+export default Stars;
