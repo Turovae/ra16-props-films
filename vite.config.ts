707285c4644e4e7d0ea4efb69064bf8ab8eaf302
Turovae <ekaterina.turova@mail.ru>
2023-05-17 17:12:02 +0700

    add deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(( {command} ) => {
+  console.log(command);
+
+  const setting = {
+    plugins: [react()],
+    base: '',
+  };
+
+  if (command === 'build') setting.base = 'https://turovae.github.io/ra16-props-films/'
+
+  return setting;
+});
