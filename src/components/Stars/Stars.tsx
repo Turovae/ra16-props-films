ab3f897628b2368771f377b9631d63afaa822663
TurovaE <ekaterina.turova@mail.ru>
2023-05-19 22:50:06 +0700

    refactoring component Stars

--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -7,22 +7,23 @@ interface StarsProp {
 
 function Stars(props: StarsProp): JSX.Element | null {
   const { count } = props;
-  if (count < 1 || count > 5) {
-    return null;
+
+  if (
+    typeof count === 'number'
+    && count >= 1
+    && count <= 5
+  ) {
+    return (
+      <>
+        <div>This is stars component with { count } stars</div>
+        <ul className="card-body-stars u-clearfix">
+          { [...Array(count).keys()].map((key) => <Star key={key} />) }
+        </ul>
+      </>
+    )
   }
 
-  if (isNaN(count)) {
-    return null;
-  }
-
-  return (
-    <>
-      <div>This is stars component with { count } stars</div>
-      <ul className="card-body-stars u-clearfix">
-        { [...Array(count).keys()].map((key) => <Star key={key} />) }
-      </ul>
-    </>
-  )
+  return null;
 }
 
 export default Stars;
